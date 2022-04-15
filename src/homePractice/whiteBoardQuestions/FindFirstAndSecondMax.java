78332ac33669602dea571edbbb22f82e77ee0bec
meldasav <gmeldag83@hotmail.com>
2022-04-15 09:01:02 -0500

    find second min

--- src/homePractice/whiteBoardQuestions/FindFirstAndSecondMax.java
+++ src/homePractice/whiteBoardQuestions/FindFirstAndSecondMax.java
@@ -2,10 +2,11 @@ package homePractice.whiteBoardQuestions;
 
 import java.util.Arrays;
 
+import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Integer.MIN_VALUE;
 
 public class FindFirstAndSecondMax {
-    // write a method Find second max in Array
+    //   write a method Find second max in Array
     public static int findSecondMax(int[] numbers) {
         int firstMax = MIN_VALUE;
         for (Integer number : numbers) {
@@ -17,7 +18,20 @@ public class FindFirstAndSecondMax {
         }
         return secondMax;
     }
+
+    public static int findSecondMinValue(int[] numbers) {
+        int firstMin = MAX_VALUE;
+        for (Integer number : numbers) {
+            if (firstMin > number) firstMin = number;
+        }
+        int secondMin = MAX_VALUE;
+        for (Integer number : numbers) {
+            if (number != firstMin && secondMin > number) secondMin = number;
+        }
+        return secondMin;
+    }
+
     public static void main(String[] args) {
-        System.out.println(findSecondMax(new int[]{5, 3, 0, -5, 10}));
+        System.out.println(findSecondMinValue(new int[]{5, 3, 2, 10}));
     }
 }
