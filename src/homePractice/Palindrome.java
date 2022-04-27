a1f52d4cf7fec935755e9c5142e3697d26d1913e
meldasav <gmeldag83@hotmail.com>
2022-04-27 15:35:17 -0500

    added changes

--- src/homePractice/Palindrome.java
+++ src/homePractice/Palindrome.java
@@ -1,9 +1,11 @@
 package homePractice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("civic"));
         System.out.println("\nTASK2\n");
 
         Scanner scan = new Scanner(System.in);
@@ -18,4 +20,25 @@ public class Palindrome {
             System.out.println("this word does not have 1 or more characters");
         }
     }
+
+
+    public static boolean isPalindrome(String str) {
+        StringBuilder sb = new StringBuilder();
+        if(!str.isEmpty()){
+            for (int i = str.length() - 1; i >= 0; i--) {
+                StringBuilder s = sb.append(str.charAt(i));
+                if (s.toString().equals(str)) return true;
+            }
+        }
+        return false;
+    }
+    public static boolean isAnagram(String str){
+        char[] array1=str.toCharArray();
+        char[] array2=str.toCharArray();
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+        return Arrays.equals(array1,array2);
+    }
+
 }
+
