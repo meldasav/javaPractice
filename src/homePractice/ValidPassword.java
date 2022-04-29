f41f3df7f64997218636454c1c76235d2e5560f8
meldasav <gmeldag83@hotmail.com>
2022-04-29 09:34:46 -0500

    changes added

--- src/homePractice/ValidPassword.java
+++ src/homePractice/ValidPassword.java
@@ -1,8 +1,14 @@
 package homePractice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class ValidPassword {
     public static void main(String[] args) {
         System.out.println(validPassword("Abcd123!"));
+        System.out.println(remove(new ArrayList(Arrays.asList("melda", "melda", "kerem", "tarik"))));
+        System.out.println(longestWord(new String[]{"yellow","blue","red"}));
     }
 
     public static boolean validPassword(String str) {
@@ -19,5 +25,19 @@ public class ValidPassword {
             else specials = true;
         }
         return upperCase && lowerCase && digit && specials;
+    }
+
+    public static ArrayList<String> remove(ArrayList<String> names) {
+        LinkedList<String> nameList = new LinkedList<>(names);
+        nameList.removeIf(name -> name.startsWith("m"));
+        return new ArrayList<>(nameList);
+    }
+
+    public static String longestWord(String[] str) {
+        String longest = "";
+        for (String s : str) {
+            if (s.length() > longest.length()) longest = s;
+        }
+        return longest;
     }
 }
