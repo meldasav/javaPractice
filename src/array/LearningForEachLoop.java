27a9c72f516d4d5a721427db5cf6d6a1e9376a1f
meldasav <gmeldag83@hotmail.com>
2022-05-01 08:30:10 -0500

    added more method

--- src/array/LearningForEachLoop.java
+++ src/array/LearningForEachLoop.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class LearningForEachLoop {
     public static void main(String[] args) {
+
         char[] chars={'A',' ','#','T','6','9'};
         for (int i = 0; i < chars.length; i++) {
             System.out.println(chars[i]);
@@ -14,4 +15,6 @@ public class LearningForEachLoop {
         }
 
     }
-}
+
+    }
+
