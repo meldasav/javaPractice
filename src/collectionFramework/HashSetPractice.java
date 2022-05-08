b94cd5f97213e74771f1d0aaea4e2a35c8bd37c9
meldasav <gmeldag83@hotmail.com>
2022-05-08 11:46:14 -0500

    last updated

--- src/collectionFramework/HashSetPractice.java
+++ src/collectionFramework/HashSetPractice.java
@@ -1,14 +1,23 @@
 package collectionFramework;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class HashSetPractice {
     public static void main(String[] args) {
+        Set<Integer> scores=new HashSet<>(Arrays.asList(5,12,10,3));
+        ArrayList<Integer> numbers1=new ArrayList<>(scores);
+        System.out.println(scores);
+        System.out.println(numbers1.get(1));
+
+        Iterator<Integer> it= scores.iterator();
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+        System.out.println(it.next());
+
+
         System.out.println(Arrays.toString(remove(new int[]{0, 5, 4, 0, 0})));
         HashSet<Integer> numbers = new HashSet<>();///there is no order you never know your elements go
-        // it will take less memory//use it with uniqe element like sos number
+        // it will take less memory//use it with unique element like sos number
         numbers.add(5);
         numbers.add(10);
         numbers.add(10);//does not allow duplicates
@@ -34,11 +43,13 @@ public class HashSetPractice {
   */
  public static Integer[] remove(int[] array){
      LinkedHashSet<Integer> set =new LinkedHashSet<>();
-     for (int i = 0; i < array.length; i++) {
-         set.add(array[i]);
+     for (int j : array) {
+         set.add(j);
      }
      return set.toArray(new Integer[0]);
  }
+
+
 
 
 }
