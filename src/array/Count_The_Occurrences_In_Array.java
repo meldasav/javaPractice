c56ba481fd868022c687186c3c676bcba2a9be23
meldasav <gmeldag83@hotmail.com>
2022-05-08 16:06:05 -0500

    recap

--- src/array/Count_The_Occurrences_In_Array.java
+++ src/array/Count_The_Occurrences_In_Array.java
@@ -9,11 +9,29 @@ public class Count_The_Occurrences_In_Array {
             if (!str.toString().contains(number + " ")) {
                 str.append(number);
                 for (int i : numbers) {
-                if (number == i) counter++;
+                    if (number == i) counter++;
                 }
             }
 
-            System.out.println("There are " + counter + "occurrences of" + number + " in the array");
+            System.out.println("There are " + counter + "occurrences of " + number + " in the array");
         }
+
+
+        int[] array = {20, 30, 10, 4, 7, 7, 3, 9, 9, 20};
+        StringBuilder sb = new StringBuilder();
+        for (int arrays : array) {
+            int count = 0;
+            if (!sb.toString().contains(arrays + "")) {
+                sb.append(arrays);
+                for (int i : array) {
+                    if (arrays == i) count++;
+                }
+            }
+
+            System.out.println("There are " + count + "occurrences of " + arrays + " in the array");
+
+        }
+
+
     }
 }
