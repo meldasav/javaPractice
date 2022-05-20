9f7bd2de613e666b62f96fc0d89650180492b923
meldasav <gmeldag83@hotmail.com>
2022-05-19 23:09:27 -0500

    array3

--- src/homePractice/CodingBadString1/Array3.java
+++ src/homePractice/CodingBadString1/Array3.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Array3 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(seriesUp(3)));
+        System.out.println((countClumps(new int[]{1,2,2,3,4,4})));
     }
        public static int[] seriesUp(int n){
            int[] array=new int[n*(n+1)/2];
@@ -16,6 +17,15 @@ public class Array3 {
                }
            }
            return array;
+       }
+       public static int countClumps(int[] num){
+        int count=0;
+           for (int i = 0; i < num.length; i++) {
+               if(i==0 || num[i]!=num[i-1] && (i!=num.length-1 && num[i]!=num[i+1])){
+                   count++;
+               }
+           }
+           return count;
        }
     }
 
