89cdda30efd5b81999f4660954f91b593857af64
meldasav <gmeldag83@hotmail.com>
2022-06-01 23:45:33 -0500

    count letter in sentence

--- src/homePractice/CountEachElementUsingMap.java
+++ src/homePractice/CountEachElementUsingMap.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class CountEachElementUsingMap {
     public static void main(String[] args) {
+        countCharacter("java is fun");
         String str = "Chicago is nice city";
         LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
         for (Character character : str.toCharArray()) {
@@ -38,29 +39,51 @@ public class CountEachElementUsingMap {
         }
         System.out.println(map3);
 
-        String str4="Git is a version control system";
-        LinkedHashMap<Character,Integer> map4= new LinkedHashMap<>();
-        for(Character c : str4.toCharArray()){
-            if(!map4.containsKey(c))map4.put(c,1);
-            else{
-                int count=map4.get(c);
-                map4.put(c,count+=1);
+        String str4 = "Git is a version control system";
+        LinkedHashMap<Character, Integer> map4 = new LinkedHashMap<>();
+        for (Character c : str4.toCharArray()) {
+            if (!map4.containsKey(c)) map4.put(c, 1);
+            else {
+                int count = map4.get(c);
+                map4.put(c, count += 1);
             }
         }
-        for(Map.Entry<Character,Integer> entry : map4.entrySet()){
+        for (Map.Entry<Character, Integer> entry : map4.entrySet()) {
             System.out.println(entry);
         }
-        String str5="Git checkout -b branch name";
-        LinkedHashMap<Character,Integer> map5=new LinkedHashMap<>();
-        for(Character c :str5.toCharArray()){
-            if(!map5.containsKey(c))map5.put(c,1);
-            else{
-                int count=map5.get(c);
-                map5.put(c,count+=1);
+        String str5 = "Git checkout -b branch name";
+        LinkedHashMap<Character, Integer> map5 = new LinkedHashMap<>();
+        for (Character c : str5.toCharArray()) {
+            if (!map5.containsKey(c)) map5.put(c, 1);
+            else {
+                int count = map5.get(c);
+                map5.put(c, count += 1);
             }
         }
-        for(Map.Entry<Character,Integer> entry : map5.entrySet()){
+        for (Map.Entry<Character, Integer> entry : map5.entrySet()) {
             System.out.println(entry);
         }
+
+        String sentence = "Definition of done is when QA is done with testing and PO approve everything to be done ";
+        LinkedHashMap<Character, Integer> map6 = new LinkedHashMap<>();
+        for (Character c : sentence.toCharArray()) {
+            if (!map6.containsKey(c)) map6.put(c, 1);
+            else map6.put(c, map6.get(c) + 1);
+        }
+        for (Map.Entry<Character, Integer> enter : map6.entrySet()) {
+            System.out.println(enter);
+        }
+    }
+
+    public static void countCharacter(String str) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for (Character c : str.toCharArray()) {
+            if (!map.containsKey(c)) map.put(c, 1);
+            else map.put(c, map.get(c) + 1);
+        }
+        for (Map.Entry<Character, Integer> enter : map.entrySet()) {
+            System.out.println(enter);
+        }
+
     }
 }
