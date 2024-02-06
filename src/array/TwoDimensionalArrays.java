cd5290ca21b7caf8dcacb85713e0675708ac44ed
meldasav <gmeldag83@hotmail.com>
2024-02-05 22:36:08 -0600

    two dimensional array

--- src/array/TwoDimensionalArrays.java
+++ src/array/TwoDimensionalArrays.java
@@ -16,9 +16,9 @@ public class TwoDimensionalArrays {
         System.out.println(studentGroups[1].length);//2
         System.out.println(studentGroups[2].length);
         //how to print the 3 th group
-        System.out.println( Arrays.toString(studentGroups[2]));
+        System.out.println(Arrays.toString(studentGroups[2]));
         System.out.println(Arrays.deepToString(studentGroups));
-        for (String[] group :studentGroups){
+        for (String[] group : studentGroups) {
             System.out.println(Arrays.toString(group));
 
 /*
@@ -43,17 +43,36 @@ public class TwoDimensionalArrays {
 
 
             System.out.println("\n------Printing all members with for each loop--------\n");
-            for(String[] group1 : studentGroups){
-                for(String member : group1){
+            for (String[] group1 : studentGroups) {
+                for (String member : group1) {
                     System.out.println(member);
                 }
             }
 
 
-            }
-                }
+        }
+        /**
+         *
+         Write the count and the average (as a double) of even numbers in a given 2-dimensional array of integers.
+         */
 
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int countEvenNumber = 0;
+        double sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] % 2 == 0) {
+                    sum += arr[i][j];
+                    countEvenNumber++;
+                }
             }
+        }
+        System.out.println("Count of even number : " + countEvenNumber);
+        System.out.println("Average: " + sum/countEvenNumber);
+    }
+
+}
 
 
 
